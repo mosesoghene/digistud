8c7afaa351405ec736dedca6d2e95076dabf2f5b
Moses Oghene <mosesogheneo@gmail.com>
2022-06-30 21:26:20 +0100

    Added a console prompt to know when file has been created

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ for x in range(2, 13):
     for y in range(1, 13):
         print(f'{x} X {y} = {x * y}', file="timestable.txt")
     print('=============================', file="timestable.txt")
+
+print("Done")
